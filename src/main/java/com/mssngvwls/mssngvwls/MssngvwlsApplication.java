e27199169f3cda8fb471490fbf6392acc07f2ce3
Stuart Leyland-Cole <stuartleyland@gmail.com>
2017-09-06 11:51:28 +0100

    Add files from Spring Boot Initialzr

---
+++ src/main/java/com/mssngvwls/mssngvwls/MssngvwlsApplication.java
@@ -0,0 +1,12 @@
+package com.mssngvwls.mssngvwls;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssngvwlsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MssngvwlsApplication.class, args);
+	}
+}
