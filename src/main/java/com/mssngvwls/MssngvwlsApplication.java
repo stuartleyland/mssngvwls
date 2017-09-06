5f7f18ce38fa730a825ac7fe40b0a2ee378853a0
Stuart Leyland-Cole <stuartleyland@gmail.com>
2017-09-06 11:56:35 +0100

    Make this a command line application and remove duplicate package

---
+++ src/main/java/com/mssngvwls/MssngvwlsApplication.java
@@ -0,0 +1,18 @@
+package com.mssngvwls;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MssngvwlsApplication implements CommandLineRunner {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(MssngvwlsApplication.class, args);
+    }
+
+    @Override
+    public void run(final String... arg0) throws Exception {
+
+    }
+}
