3c4483bd65cba63521ca4c81b6b851a4eaf353fc
Stuart Leyland-Cole <stuartleyland@gmail.com>
2017-09-20 16:23:33 +0100

    Remove commented out code and command line game

--- src/main/java/com/mssngvwls/MssngvwlsApplication.java
+++ src/main/java/com/mssngvwls/MssngvwlsApplication.java
@@ -4,47 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MssngvwlsApplication { //implements CommandLineRunner {
-
-    //    private final GameFactory gameFactory;
-    //
-    //    public MssngvwlsApplication(final GameFactory gameFactory) {
-    //        this.gameFactory = gameFactory;
-    //    }
+public class MssngvwlsApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(MssngvwlsApplication.class, args);
     }
-
-    //    @Override
-    //    public void run(final String... arg0) throws Exception {
-    //        final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    //        final Game game = gameFactory.createGame();
-    //        GameState gameState = game.startGame(2, 2);
-    //        final Queue<GamePhrase> gamePhrases = gameState.getPhrases();
-    //        System.out.println("Number of Phrases remaining: " + gamePhrases.size());
-    //
-    //        while (!gameState.isGameOver()) {
-    //            displayGameState(gameState);
-    //            final String guess = br.readLine();
-    //            gameState = game.guessPhrase(guess);
-    //        }
-    //
-    //        System.out.println("*** Game Over ***");
-    //        displayGameState(gameState);
-    //    }
-    //
-    //    private void displayGameState(final GameState gameState) {
-    //        final Optional<Boolean> previousGuessCorrect = gameState.getPreviousGuessCorrect();
-    //        if (previousGuessCorrect.isPresent()) {
-    //            System.out.println("Previous guess correct? " + previousGuessCorrect.get());
-    //        }
-    //
-    //        final Optional<String> currentCategory = gameState.getCurrentCategory();
-    //        System.out.println("Category: " + (currentCategory.isPresent() ? currentCategory.get() : "<no category>"));
-    //        final Optional<GamePhrase> currentPhrase = gameState.getCurrentPhrase();
-    //        System.out
-    //                .println("Phrase: " + (currentPhrase.isPresent() ? currentPhrase.get().getPhraseWithoutVowels() : "<no phrase>"));
-    //        System.out.println("Score: " + gameState.getScore());
-    //    }
 }
