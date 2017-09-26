62003745b8247c27a8533da1190202c50535c1ef
Stuart Leyland-Cole <stuartleyland@gmail.com>
2017-09-26 14:56:56 +0100

    Test to ensure context loads

---
+++ src/test/java/com/mssngvwls/test/MssngvwlsApplicationTests.java
@@ -0,0 +1,16 @@
+package com.mssngvwls.test;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MssngvwlsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
